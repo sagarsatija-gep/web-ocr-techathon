b1c58352288e234cbf199165fd0be1eb1c1a6cde
NagaSaranyaKunapuli-gep <68947593+NagaSaranyaKunapuli-gep@users.noreply.github.com>
2020-10-31 19:01:59 +0530

    row click added

--- src/app/dashboard-page/dashboard-page.component.ts
+++ src/app/dashboard-page/dashboard-page.component.ts
@@ -1,10 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {MatTableModule} from '@angular/material/table';
-import { MatTable } from '@angular/material/table';
-import { MatTableDataSource } from '@angular/material/table';
-// import "../node_modules/ag-grid-community/dist/styles/ag-grid.css";
-// import "../node_modules/ag-grid-community/dist/styles/ag-theme-alpine-dark.css";
-// import "../node_modules/ag-grid-community/dist/styles/ag-theme-alpine.css";
+import { Router } from '@angular/router';
 import { FileSelectDirective, FileUploader} from 'ng2-file-upload';
 
 const uri = 'http://localhost:4200/file/upload';
@@ -19,7 +14,7 @@ export class DashboardPageComponent {
 
     attachmentList:any = [];
 
-    constructor(){
+    constructor(private route: Router){
 
         this.uploader.onCompleteItem = (item:any, response:any , status:any, headers:any) => {
             this.attachmentList.push(JSON.parse(response));
@@ -35,13 +30,23 @@ export class DashboardPageComponent {
   onUpload() {
     // upload code goes here
   }
+  onRowClicked(event: any) 
+  { 
+    let id=event.data.invoiceNo;
+    this.route.navigate([`/details-ocr/${id}`])
+    console.log(id);
+    console.log('row', event); 
+   }
 
   columnDefsAll = [
     { headerName:'invoiceNo',
-      field: 'invoiceNo' },
-
-    { field: 'userName' },
-    { field: 'status'}
+      field: 'invoiceNo' ,
+      rowSelection:'single'
+  },
+    { headerName:'userName',
+      field: 'userName' },
+    { headerName:'status',
+      field: 'status'}
 ];
 
 rowDataAll = [
